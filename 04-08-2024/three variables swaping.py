68b66f6cd59b78b658bf931b815c7a31df91fcd2
bhaskar9034 <mutyalusrkrec850@gmail.com>
2024-08-04 23:19:17 +0530

    addedprint statements

--- 04-08-2024/three variables swaping.py
+++ 04-08-2024/three variables swaping.py
@@ -1,3 +1,16 @@
 a = int(input("Enter a num: "))
 b = int(input("Enter b num: "))
 c = int(input("Enter c num: "))
+
+print("\nThe value of a before swaping is ",a)
+print("The value of b before swaping is ",b)
+print("The value of c before swaping is ",c)
+
+temp = a
+a = b
+b = c
+c = temp
+
+print("\nThe value of a after swaping is ",a)
+print("The value of b after swaping is ",b)
+print("The value of c after swaping is ",c)
