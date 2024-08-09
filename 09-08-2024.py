d3b2ee00d0ee9e16e4d5500ff8146f0faaaed801
bhaskar9034 <mutyalusrkrec850@gmail.com>
2024-08-09 22:48:06 +0530

    adding new element operation

--- 09-08-2024.py
+++ 09-08-2024.py
@@ -2,5 +2,8 @@ my_tuple = 1,2,3    #By default this will be considered as tuple, without using 
 list_conversion = set(my_tuple)
 print(list_conversion)
 
-set1 = {1,2,1}
+set1 = {1,2,1}    #ignores repeated values
+print(set1)
+
+set1.add(4)    #adding new element into set1
 print(set1)
