75f796556ad53dd344032bc92d838255476e09ad
bhaskar9034 <mutyalusrkrec850@gmail.com>
2024-08-10 22:23:50 +0530

    performing set comprehnsion to create another set

--- 10-08-2024/set_comprehension.py
+++ 10-08-2024/set_comprehension.py
@@ -3,3 +3,11 @@
 printing a particular elements from a pre-existing list.
 here we have to print the elements the elements consisting 'g'
 '''
+# the below block of code will retrive every element which has g in it.
+
+companys = {'google','goldman sachas','jaguar','titan'} 
+g_company = []
+for company in companys:
+    if 'g' in company:
+        g_company.append(company)
+print(g_company)
