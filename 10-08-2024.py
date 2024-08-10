2c4345f4b2956a86c9669e7405b452279c79ca71
bhaskar9034 <mutyalusrkrec850@gmail.com>
2024-08-10 19:15:22 +0530

    using in and len keywords in python

--- 10-08-2024.py
+++ 10-08-2024.py
@@ -1,1 +1,7 @@
 # Continution to 09-08-2024.py
+my_list = {1,2,3}
+
+print(3 in my_list)    #checks the given value is present in the list if present returns True else False
+print(4 in my_list)
+
+print(_len(my_list))    #Gives the length of list
