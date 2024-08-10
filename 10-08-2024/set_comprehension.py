ebfb91052a7c8137f0f942b6772bfa3e5cb56a09
bhaskar9034 <mutyalusrkrec850@gmail.com>
2024-08-10 22:51:05 +0530

    deleted empty gaps

--- 10-08-2024/set_comprehension.py
+++ 10-08-2024/set_comprehension.py
@@ -1,8 +1,11 @@
 # Set comprehenison means..cretaing a list from pre-existing list
+
 '''
 printing a particular elements from a pre-existing list.
 here we have to print the elements the elements consisting 'g'
 '''
+
+
 # the below block of code will retrive every element which has g in it.
 
 companys = {'google','goldman sachas','jaguar','titan'} #creating a set
@@ -11,8 +14,6 @@ for company in companys: #we are storing each element in the list of companys as
     if 'g' in company:    #check the element contains 'g' in it or not 
         g_company.add(company)   #if the element contains 'g' then add it to the list
 print(g_company)    #print the company names starting with 'g'
-
-
 
 
 # the below block of code will create an set of elements starting with 'g'
