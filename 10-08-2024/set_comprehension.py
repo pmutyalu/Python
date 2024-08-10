310b4beeb2c44e424e66c7f35949a1f4680931d8
bhaskar9034 <mutyalusrkrec850@gmail.com>
2024-08-10 22:15:52 +0530

    added documentation

--- 10-08-2024/set_comprehension.py
+++ 10-08-2024/set_comprehension.py
@@ -1,1 +1,5 @@
 # Set comprehenison means..cretaing a list from pre-existing list
+'''
+printing a particular elements from a pre-existing list.
+here we have to print the elements the elements consisting 'g'
+'''
