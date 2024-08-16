cfbb078b15a53dc786bbe0b5c75e63f10a182eb7
bhaskar9034 <mutyalusrkrec850@gmail.com>
2024-08-16 22:32:02 +0530

    question

---
+++ 15-08-2024.py
@@ -0,0 +1,15 @@
+'''
+
+Given a sorted array of N integers and an integer X, find the integer in array that is closest to X. If there are multiple such integers, print the smaller number.
+
+Closeness of two integers u and v is simply absolute value of u - v.
+
+Input
+First line contains two integers N and X.
+
+Second line contains N integers describing the array.
+
+Output
+Print smallest integer in array that is closest to X.
+
+'''
