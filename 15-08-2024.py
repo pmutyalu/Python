e7f50301b87e035a040b42f719ca635e6a1e47dc
bhaskar9034 <mutyalusrkrec850@gmail.com>
2024-08-16 23:34:32 +0530

    answer

--- 15-08-2024.py
+++ 15-08-2024.py
@@ -13,3 +13,18 @@ Output
 Print smallest integer in array that is closest to X.
 
 '''
+
+
+x = int(input('Enter an integer: '))
+arr = [3, 2, 8, 5, 6]
+arr.sort()  # Sorting the list
+closest = arr[0]
+smallest = abs(closest - x)
+
+for i in arr:
+    current_diff = abs(i - x)    #useing abs to make incase of negative value occurance can be changed to positive
+    if current_diff < smallest:
+        closest = i
+        smallest = current_diff
+
+print("The closest number to", x, "is", closest)
