09f8260dd6e2b45dff95c94e28a13e307217cf73
bhaskar9034 <mutyalusrkrec850@gmail.com>
2024-08-18 21:22:32 +0530

    SAMPLE DICTIONARY

--- 17-08-2024.py
+++ 17-08-2024.py
@@ -6,3 +6,6 @@ A dictionary is an unordered collection of key-value pairs. Are used to store da
 Creating Dictionaries
 To create a dictionary, you use curly braces {} and specify key-value pairs separated by colons :. Keys and values can be of any data type.
 '''
+
+sample_dict = {'Dtype': 'string','Dtype_boolean': True, 'Dtype_Integer': 850}
+print(sample_dict)
