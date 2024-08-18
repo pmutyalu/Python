3fbd03d1f7a6e43977debca3aa6c5a8cdfbe9cbd
bhaskar9034 <mutyalusrkrec850@gmail.com>
2024-08-18 21:17:44 +0530

    Creating Dictionaries

--- 17-08-2024.py
+++ 17-08-2024.py
@@ -1,3 +1,8 @@
 '''
 A dictionary is an unordered collection of key-value pairs. Are used to store data in the form of key-value pairs, where each key is unique.
 '''
+
+'''
+Creating Dictionaries
+To create a dictionary, you use curly braces {} and specify key-value pairs separated by colons :. Keys and values can be of any data type.
+'''
