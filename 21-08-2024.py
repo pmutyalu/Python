5b963d35e4afa6070169bd1816260518d8aaff53
bhaskar9034 <mutyalusrkrec850@gmail.com>
2024-08-22 21:25:16 +0530

    inding biggest Ele

--- 21-08-2024.py
+++ 21-08-2024.py
@@ -5,3 +5,8 @@ Finding biggest and smallest in a given list of elements.
 arr = [12,3,36,2,9,29]
 big = arr[0]
 small = arr[0]
+
+for i in arr:
+    if big < i:
+        big  = i
+print('Biggest element in the list is: ',big)
