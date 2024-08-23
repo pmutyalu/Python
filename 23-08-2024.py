dc4582aa543673f81e5aace400013aabd90896b6
bhaskar9034 <mutyalusrkrec850@gmail.com>
2024-08-23 22:38:15 +0530

    converting list to set

--- 23-08-2024.py
+++ 23-08-2024.py
@@ -3,3 +3,7 @@ Removeing duplicates.
 '''
 
 a_list = [10,20,10,20,30,40,10,50]
+
+a_set = set(a_list)    #if we store an list consisting duplicate elements then all repeated elements will be removed.
+
+print(a_set)
