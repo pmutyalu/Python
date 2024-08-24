40b0de255a87698130bd7515912ad8bbfec00bfa
bhaskar9034 <mutyalusrkrec850@gmail.com>
2024-08-24 22:44:51 +0530

    Removeing duplicates.  (Errors occuring)

--- 23-08-2024.py
+++ 23-08-2024.py
@@ -6,4 +6,16 @@ a_list = [10,20,10,20,30,40,10,50]
 
 a_set = set(a_list)    #if we store an list consisting duplicate elements then all repeated elements will be removed.
 
+original_list =a_set.sort()
+print(a_set)
+
+'''
+Removeing duplicates.
+'''
+
+a_list = [10,20,10,20,30,40,10,50]
+
+a_set = set(a_list)    #if we store an list consisting duplicate elements then all repeated elements will be removed.
+
+original_list =a_set.sort()
 print(a_set)
