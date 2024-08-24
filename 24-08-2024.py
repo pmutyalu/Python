74b59f3e4f97a1e3799163a0c824ff966ac1998e
bhaskar9034 <mutyalusrkrec850@gmail.com>
2024-08-24 22:47:31 +0530

    test case1

--- 24-08-2024.py
+++ 24-08-2024.py
@@ -2,3 +2,7 @@
 problem statement:
 You're going to write some code to help you cook a gorgeous lasagna from your favorite cookbook. You have five tasks, all related to cooking your recipe.
 '''
+
+#Test_case1: define the 'EXPECTED_BAKE_TIME' constant.
+
+EXPECTED_BAKE_TIME = 40
