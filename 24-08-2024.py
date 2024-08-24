b393084ea012b7058dea9ad3a492a65c8d9a698d
bhaskar9034 <mutyalusrkrec850@gmail.com>
2024-08-24 22:49:56 +0530

    Tet_case 4

--- 24-08-2024.py
+++ 24-08-2024.py
@@ -33,3 +33,19 @@ def preparation_time_in_minutes(number_of_layers):
     Assumes each layer takes 'PREPARATION_TIME_PER_LAYER' minutes to prepare.
     """
     return number_of_layers*2
+
+
+#TODO: define the 'elapsed_time_in_minutes()' function below.
+def elapsed_time_in_minutes(number_of_layers, elapsed_bake_time):
+    """Calculate the elapsed cooking time.
+
+    :param number_of_layers: int - the number of layers in the lasagna.
+    :param elapsed_bake_time: int - elapsed cooking time.
+    :return: int - total time elapsed (in minutes) preparing and cooking.
+
+    This function takes two integers representing the number of lasagna layers and the
+    time already spent baking and calculates the total elapsed minutes spent cooking the
+    lasagna.
+    """
+    number_of_layers = preparation_time_in_minutes(number_of_layers)
+    return elapsed_bake_time + number_of_layers
