c1bc64aee54fccf42dd42a98ea9df9d271d08f03
bhaskar9034 <mutyalusrkrec850@gmail.com>
2024-08-25 14:45:35 +0530

    Test_case-1

--- 25-08-2024.py
+++ 25-08-2024.py
@@ -1,3 +1,16 @@
 ''' Instructions
 Your friend Chandler plans to visit exotic countries all around the world. Sadly, Chandler's math skills aren't good. He's pretty worried about being scammed by currency exchanges during his trip - and he wants you to make a currency calculator for him. Here are his specifications for the app:
 '''
+
+
+
+def exchange_money(budget, exchange_rate):
+    """
+
+    :param budget: float - amount of money you are planning to exchange.
+    :param exchange_rate: float - unit value of the foreign currency.
+    :return: float - exchanged value of the foreign currency you can receive.
+    """
+    return budget/exchange_rate
+exchange_money(127.5, 1.2)
+    
