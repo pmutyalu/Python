ab4aeed417d7d11a370b39726e23812d4bbe1f93
bhaskar9034 <mutyalusrkrec850@gmail.com>
2024-08-25 14:46:01 +0530

    Test_case-2

--- 25-08-2024.py
+++ 25-08-2024.py
@@ -14,3 +14,13 @@ def exchange_money(budget, exchange_rate):
     return budget/exchange_rate
 exchange_money(127.5, 1.2)
     
+def get_change(budget, exchanging_value):
+    """
+
+    :param budget: float - amount of money you own.
+    :param exchanging_value: float - amount of your money you want to exchange now.
+    :return: float - amount left of your starting currency after exchanging.
+    """
+    return budget - exchanging_value
+get_change(127.5, 120)
+    
