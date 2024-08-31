61c198a186f88b826a86dbd4f9a992a5baed552a
bhaskar9034 <mutyalusrkrec850@gmail.com>
2024-08-31 22:37:51 +0530

    Test_Function-2

--- 31-08-2021.py
+++ 31-08-2021.py
@@ -24,3 +24,10 @@ raise ValueError("square must be between 1 and 64")
 
 def square(number):
     return 2**(number-1)
+
+
+def total():
+    sum = 0
+    for i in range(1,65):
+        sum += square(i)
+    return sum
