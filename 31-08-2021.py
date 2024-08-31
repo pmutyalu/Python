386585d0f684fb40df8f93a509cb4c11de0fae63
bhaskar9034 <mutyalusrkrec850@gmail.com>
2024-08-31 22:38:37 +0530

    Adding Exception

--- 31-08-2021.py
+++ 31-08-2021.py
@@ -23,6 +23,8 @@ raise ValueError("square must be between 1 and 64")
 
 
 def square(number):
+    if number<1 or number>64:
+        raise ValueError("square must be between 1 and 64")
     return 2**(number-1)
 
 
