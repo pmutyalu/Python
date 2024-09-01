d79b7fa0989cf0de967df5f9eea667af93103075
bhaskar9034 <mutyalusrkrec850@gmail.com>
2024-09-01 12:45:33 +0530

    Task-3

--- 01-09-2024.py
+++ 01-09-2024.py
@@ -42,3 +42,7 @@ def eat_ghost(power_pellet_active, touching_ghost):
 
 def score(touching_power_pellet, touching_dot):
     return touching_power_pellet or touching_dot
+
+def lose(power_pellet_active, touching_ghost):
+    return not power_pellet_active and touching_ghost
+
