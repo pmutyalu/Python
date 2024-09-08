31c0e804d9758259c65c93abb532e40e2c5de36f
bhaskar9034 <mutyalusrkrec850@gmail.com>
2024-09-08 17:55:04 +0530

    added documentation

--- 08-09-2024.py
+++ 08-09-2024.py
@@ -1,3 +1,15 @@
 a = 5
 b = 0
-print(a/b)
+'''
+All the above statements are fine and there is no chance of generating an error(These are just declaration var's). 
+But if we perform division operation on variables then we can't divide with zero, so there is an chance of error here we can write the statements which have chance of error in try statements.
+'''
+try:
+    print(a/b)
+    
+    
+    """
+    This is just printing we got an error we don't know exact name of error.
+    """
+except Exception:
+    print('The second variable is zero, can\'t divide with zero')
