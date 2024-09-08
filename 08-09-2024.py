5726918ce71c7dd8f51cefecdc8fa0a3c453a15c
bhaskar9034 <mutyalusrkrec850@gmail.com>
2024-09-08 19:01:03 +0530

    adding finally!

--- 08-09-2024.py
+++ 08-09-2024.py
@@ -5,12 +5,22 @@ All the above statements are fine and there is no chance of generating an error(
 But if we perform division operation on variables then we can't divide with zero, so there is an chance of error here we can write the statements which have chance of error in try statements.
 '''
 try:
+    print('helllo this is before exception raised')
     print(a/b)
-    
+    """
+    we can perform any oprations in try block as normally but whenever a lines hits the error below statements will not be executed.
+    To overcome such problem we have to use different try and except blocks
+    """
     
     """
     This is just printing we got an error we don't know exact name of error.
     if we print custom error message along with the error name we should write the following. 
     """
-except Exception as e:
-    print('The second variable is zero, can\'t divide with zero\n',e)
+except ZeroDivisionError:
+    print('The second variable is zero, can\'t divide with zero\n')
+    
+    """
+    Finally will get executed no matter what the above code
+    """
+finally:
+    print('helllo this is after exception raised')
