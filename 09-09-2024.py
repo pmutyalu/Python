2a4e38c2200b0ed23b6c34b8bc78269e3cc45761
bhaskar9034 <mutyalusrkrec850@gmail.com>
2024-09-13 21:35:00 +0530

    documenation

--- 09-09-2024.py
+++ 09-09-2024.py
@@ -1,9 +1,9 @@
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt    #matplotlib is an visualization library.
 
 x = [1,2,3,4,5]
 y = [6,7,8,9,10]
 
-plt.plot(x,y,marker = 'o', ms=30)
+plt.plot(x,y,marker = 'o', ms=30)   #plotting x & y coordinates. marker isused to indicate the datapoints with custom shapess
 plt.title('sample plot')
 plt.show()
 
