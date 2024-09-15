a6e9873ddbefc40dfb17a211c08e6d0fb323abe6
bhaskar9034 <mutyalusrkrec850@gmail.com>
2024-09-15 21:24:52 +0530

    Challenge-2

--- 15-09-2024_HackerRank.py
+++ 15-09-2024_HackerRank.py
@@ -3,7 +3,6 @@
 Let's learn about list comprehensions! You are given three integers x, y and z representing the dimensions of a cuboid along with an integer n. Print a list of all possible coordinates given by (i, j, k) on a 3D grid where the sum of i + j + k is not equal to n. Here, 0 i c; 0 j y; 0 k z. Please use list comprehensions rather than multiple loops, as a learning exercise. Example
 All permutations of [i, j, k] are:
 
-
 Sample Input:
 1
 1
@@ -23,3 +22,11 @@ result = [[i,j,k]
           if x+y+z!=2
 ]
 print(result)
+
+
+'''-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------'''
+
+
+'''Given the participants' score sheet for your University Sports Day, you are required to find the runner-up score. You are given n scores. Store them in a list and find the score of the runner-up.
+Input Format The first line contains n. The second line contains an array of n integers each separated by a space.
+Given list is [2, 3, 6, 6, 5]. The maximum score is 6, second maximum is 5. Hence, we print 5 as the runner-up score.'''
