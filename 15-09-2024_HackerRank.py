6e231600938744bb119997544183fc9fa420684d
bhaskar9034 <mutyalusrkrec850@gmail.com>
2024-09-15 21:11:34 +0530

    Adding Question

---
+++ 15-09-2024_HackerRank.py
@@ -0,0 +1,14 @@
+#Challenge-1
+'''
+Let's learn about list comprehensions! You are given three integers x, y and z representing the dimensions of a cuboid along with an integer n. Print a list of all possible coordinates given by (i, j, k) on a 3D grid where the sum of i + j + k is not equal to n. Here, 0 i c; 0 j y; 0 k z. Please use list comprehensions rather than multiple loops, as a learning exercise. Example
+All permutations of [i, j, k] are:
+
+
+Sample Input:
+1
+1
+1
+2
+Sample Output:
+[0, o, 0], [o, o, 1], [0, 1, o, n, o, 0], n, 1, 11]
+'''
