4bcb79179b0aac00206a912a5990344a429c3e92
bhaskar9034 <mutyalusrkrec850@gmail.com>
2024-09-23 21:38:07 +0530

    Updated documentation

--- 26-08-2024.py
+++ 26-08-2024.py
@@ -30,7 +30,8 @@ print(str_Repetation)
 '''
 Nested list has another list in the original list.
 In the below esxample 0,1,2,4 indices contains normal elements. But 3rd index contains another list of n elements. all the elements in the sub list will come under index of 3.
-Using slicing we can acces the sublist or we can access them useing their index positions.'''
+Using slicing we can acces the sublist or we can access them useing their index positions.
+'''
 nested_list = [10,30,8,['-10',102,49],24]
 print(nested_list[3][::])  #accesing all elements in neste list. also we can write nested_list[3] to get same result.
 print(nested_list[3][::2])  #performing slicing on nested list. here we are accessing all elements of an list with step count of 2. this will give each elements with two index difference between each.
