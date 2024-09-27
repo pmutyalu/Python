3ae228b2792f0d7fd80050d087cac066e4614411
bhaskar9034 <mutyalusrkrec850@gmail.com>
2024-09-27 12:58:28 +0530

    Solution

--- Nqt prev ques.py
+++ Nqt prev ques.py
@@ -1,3 +1,15 @@
 '''
 Write a program to generate the Fibonacci series and find the sum of elements up to N terms.
 '''
+
+
+def fib_cal(n):
+    total = 0
+    a,b = 0,1
+    for _ in range(n):
+        print(a,end=' ')
+        total += a
+        a,b = b,a+b
+    print('\nThe toatl sum\'s of fib is: ',total)
+n = int(input('Enter a num to find fib : '))
+fib_cal(n)
